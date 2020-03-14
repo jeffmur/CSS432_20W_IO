97ab03320de28e556128ab3f528c6347b3bd9b96
jeffmur <jeffmur@uw.edu>
2020-03-14 04:56:14 -0700

    Ready for merge

--- Checkers/Assets/Scripts/GameStat.cs
+++ Checkers/Assets/Scripts/GameStat.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameStat : MonoBehaviour
 {
+    private Client client;
+
+    public Text showDown;
+    public string whoAmI;
+    public string opponent;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (GameManager.Instance != null)
+            client = GameManager.Instance.clientObject.GetComponent<Client>();
+        else
+            Debug.LogError("GameManager is null at start of Game!");
+    }
+
+    private void Start()
+    {
+        OpponentPopUp();
     }
 
     // Update is called once per frame
@@ -17,13 +31,30 @@ public class GameStat : MonoBehaviour
         
     }
 
+    public void OpponentPopUp()
+    {
+        opponent = GameManager.Instance.oponentUsername;
+        whoAmI = client.clientName;
+        // only show for online
+        if (opponent != "" && whoAmI != "")
+            showDown.text = $"{whoAmI} vs {opponent}";
+        else
+            showDown.text = "Practice";
+            
+        StartCoroutine(hideAfter(showDown.transform.parent.gameObject, 2));
+    }
+
+    private IEnumerator hideAfter(GameObject toHide, int sec)
+    {
+        yield return new WaitForSeconds(sec);
+        toHide.SetActive(false);
+    }
+
     public void Quit()
     {
         // Close socket if online
-        if(GameManager.Instance != null)
-        {
-            GameManager.Instance.clientObject.GetComponent<Client>().CloseSocket();
-        }
+        if(client != null)
+            client.CloseSocket();
         SceneManager.LoadScene("Menu");
     }
 }
